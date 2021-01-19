0f7ec1f079edc75a902e12e4aa1e324d0ee1c048
zakuraevs <zakuraevs@gmail.com>
2021-01-19 12:50:06 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/basic_miltifile_app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic_miltifile_app.dir/main.cpp.o"
+  "basic_miltifile_app"
+  "basic_miltifile_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_miltifile_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
